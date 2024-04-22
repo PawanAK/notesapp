9d447059776d9631f6a26e71506c11d06f58a03d
PawanAK <pawanajjark@gmail.com>
2024-04-22 20:52:17 +0530

    add Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
 import { getUser } from "@/lib/auth";
+import { Lilita_One } from "next/font/google";
 import React from "react";
+
+const lilita = Lilita_One({ weight: "400", subsets: ["latin"] });
 
 const Header = async () => {
   const user = await getUser();
   return (
     <div className="relative mt-2 flex h-20 w-full max-w-5xl items-center justify-between rounded-lg bg-popover px-4">
-      <h1>Notees</h1>
+      <UserButton />
+      <h1 className={`text-4xl text-secondary sm:text-5xl ${lilita.className}`}>
+        Notees
+      </h1>
+      <NewNoteButton />
     </div>
   );
 };
