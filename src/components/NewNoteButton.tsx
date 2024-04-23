3b0f54962be3dae859576c0be22b47e204dbbeed
PawanAK <pawanajjark@gmail.com>
2024-04-23 11:03:16 +0530

    add  addButton

--- src/components/NewNoteButton.tsx
+++ src/components/NewNoteButton.tsx
@@ -1,11 +1,19 @@
+"use client";
 import React from "react";
 import { Dialog, DialogTrigger } from "./ui/dialog";
 import { Plus } from "lucide-react";
+import NewNoteDialog from "./NewNoteDialog";
+import { cn } from "@/lib/utils";
 
-const NewNoteButton = () => {
+const NewNoteButton = ({ className }: { className?: String }) => {
   return (
     <Dialog>
-      <DialogTrigger>
+      <DialogTrigger
+        className={cn(
+          "text-secondary transition-colors duration-200 ease-in-out hover:text-primary",
+          className,
+        )}
+      >
         <Plus />
       </DialogTrigger>
       <NewNoteDialog />
